c4c3014df3c751e732f600126ef6a9eeb22b9580
Do Vu Truong <56952469+dotruong1994@users.noreply.github.com>
2022-12-24 16:02:11 +0700

    Update code 24/12/2022
    A thôn lành nhắc

--- JSON_IOT_DONE_BUZZER_IP74_ver20.ino
+++ JSON_IOT_DONE_BUZZER_IP74_ver20.ino
@@ -10,17 +10,19 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);
 unsigned long dem =0;
 unsigned long dem2 =0;    
 unsigned long dem3 =0;          // các biếm tạm để gán cho việc sử dụng đèn
-unsigned long dem4 =0;        
+unsigned long dem4 =0;
+unsigned long count_1 = 1000;
+long lastMsg = 0;        
 bool tam=0;
 bool tam2=0;
 bool tam3=0;
 bool checkLEDVal;
+const uint32_t connectTimeoutMs = 10000;
 int x=0;
-#define NUM_LEDS 7
+#define NUM_LEDS 51
 #define DATA_PIN 5
 CRGB leds[NUM_LEDS];
-#define LED_PIN     GPIO_NUM_27
-
+#define LED_PIN GPIO_NUM_27
 
 #include <WiFi.h>
 #include <WiFiMulti.h>    // khai báo khối wifi và http service
@@ -37,7 +39,7 @@ EasyButton button2(BUTTON_PIN2);
 EasyButton button3(BUTTON_PIN3); 
 EasyButton button4(BUTTON_PIN4); 
 
-String ID = "A6-MK-23" ; // thay đổi ID vị trí ở chỗ này   
+String ID = "11223344" ; // thay đổi ID vị trí ở chỗ này   
 String rfid;
 
 // khối chương trình con chức năng cho các nút nhấn
@@ -55,7 +57,7 @@ void check_RFID();
 
 void dencanbo()
 {
-    for(int i=0;i<7;i++)
+    for(int i=0;i<51;i++)
       {
         leds[i] = CRGB(90,32,167);
         FastLED.show();
@@ -63,7 +65,7 @@ void dencanbo()
 }
 void denbaotri()
 {
-    for(int i=0;i<7;i++)
+    for(int i=0;i<51;i++)
       {
         leds[i] = CRGB(0,255,20);
         FastLED.show();
@@ -71,7 +73,7 @@ void denbaotri()
 }
 void denhoatdong()
 {
-    for(int i=0;i<7;i++)
+    for(int i=0;i<51;i++)
       {
         leds[i] = CRGB(236,236,236);
         FastLED.show();
@@ -79,7 +81,7 @@ void denhoatdong()
 }
 void den_nhan_don()
 {
-    for(int i=0;i<7;i++)
+    for(int i=0;i<51;i++)
       {
         leds[i] = CRGB(255, 0, 0);
         FastLED.show();
@@ -87,26 +89,48 @@ void den_nhan_don()
 }
 void denhetlieu()
 {
-    for(int i=0;i<7;i++)
+    for(int i=0;i<51;i++)
       {
         leds[i] = CRGB(249,244,0);
         FastLED.show();
       } 
 }
 
-// chương trình còn giám sát wifi
+void den_trieu_hoi_thanh_dong()
+{
+    for(int i=0;i<51;i++)
+      {
+        leds[i] = CRGB(0,75,130);
+        FastLED.show();
+      } 
+}
+
+
+// chương trình con giám sát wifi
 
 void monitorWiFi(void * parameter){
   for(;;){ // infinite loop
-    if(millis()-dem4>=10000)
-     {
-        if((wifiMulti.run() != WL_CONNECTED)){
-            Serial.println("Reset ESP");
+    if(millis()-count_1>=10000)
+    {
+      if (wifiMulti.run(connectTimeoutMs) == WL_CONNECTED) 
+      {
+        Serial.print("WiFi connected: ");
+        Serial.print(WiFi.SSID());
+        Serial.print(" ");
+        Serial.println(WiFi.RSSI());
+      }
+      else {
+        Serial.println("WiFi not connected!");
+        den_trieu_hoi_thanh_dong();
+        long now = millis();
+        if (now - lastMsg > 20000) 
+          {
+            lastMsg = now;
             ESP.restart();
-            }
-          dem4=millis();
-     }
-    vTaskDelay(10);
+          }
+      }
+    }
+    count_1=millis();
   }
 }
 void andonLoop(void * parameter){
@@ -115,10 +139,7 @@ void andonLoop(void * parameter){
     button2.read();
     button3.read();
     button4.read();
-    button1.onPressed(receive_order);
-    button2.onPressed(leader);
-    button3.onPressed(material);
-    button4.onPressed(call_tpm);
+
     check_RFID();
     if(checkLEDVal==1)
       {
@@ -167,13 +188,13 @@ void andonLoop(void * parameter){
     }
     vTaskDelay(10);
   }
+//  Serial.println(uxTaskGetStackHighWaterMark());
 }
 
 
 void setup()
 {
   SPI.begin(); 
-  //WiFi.mode(WIFI_STA);
   vTaskDelay(500);
   mfrc522.PCD_Init(); 
   vTaskDelay(50);
@@ -181,16 +202,26 @@ void setup()
   button2.begin();
   button3.begin();
   button4.begin();
+  button1.onPressed(receive_order);
+  button2.onPressed(leader);
+  button3.onPressed(material);
+  button4.onPressed(call_tpm);
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
   Serial.begin(115200);
   FastLED.addLeds<WS2813, DATA_PIN, RGB>(leds, NUM_LEDS);
-
-  //wifiMulti.addAP("steve", "11223344");
-  wifiMulti.addAP("Redmi K30 5G Speed", "88888888");
+  FastLED.setBrightness(255);
+  WiFi.mode(WIFI_STA);
+  wifiMulti.addAP("Redmi", "thanh12345");
+  wifiMulti.addAP("AE_CAM", "Aa123456");
+  wifiMulti.addAP("OPPO F9", "thuat12345678!");
   wifiMulti.addAP("factory-A10-1F-3", "qwerasdf");
   wifiMulti.addAP("factory-A10-1F-2", "qwerasdf");
   wifiMulti.addAP("factory-A10-1F-1", "qwerasdf");
+
+  wifiMulti.addAP("Factory-A1-1F-3", "qwerasdf");
+  wifiMulti.addAP("Factory-A1-1F-2", "qwerasdf");
+  wifiMulti.addAP("Factory-A1-1F-1", "qwerasdf");
 
   wifiMulti.addAP("factory-A6-1F-3", "qwerasdf");
   wifiMulti.addAP("factory-A6-1F-2", "qwerasdf");
@@ -224,11 +255,19 @@ void setup()
 
   while (wifiMulti.run() != WL_CONNECTED) 
   {
+    den_trieu_hoi_thanh_dong();
     digitalWrite(LED_PIN,HIGH);
     vTaskDelay(500);
     digitalWrite(LED_PIN,LOW);
     vTaskDelay(500);
     Serial.println("Connecting to WiFi..");
+    long now = millis();
+    if (now - lastMsg > 20000) {
+      lastMsg = now;
+      ESP.restart();
+  }
+
+
   }
     Serial.println("");
     Serial.println("WiFi connected");
@@ -243,7 +282,7 @@ void setup()
   xTaskCreatePinnedToCore(
     monitorWiFi,    // Function that should be called
     "Monitor Wifi",   // Name of the task (for debugging)
-    64000,            // Stack size (bytes)
+    8192,            // Stack size (bytes)
     NULL,            // Parameter to pass
     1,               // Task priority
     NULL,             // Task handle
@@ -253,7 +292,7 @@ void setup()
   xTaskCreate(
     andonLoop,    // Function that should be called
     "andonLoop",   // Name of the task (for debugging)
-    64000,            // Stack size (bytes)
+    8192,            // Stack size (bytes)
     NULL,            // Parameter to pass
     2,               // Task priority
     NULL             // Task handle
@@ -263,15 +302,18 @@ void setup()
 void loop() 
 {
 }
-
 void check_RFID(){
- if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  if (mfrc522.PICC_IsNewCardPresent()) 
   { 
-    checkLEDVal=0;
-    return;
+    if (mfrc522.PICC_ReadCardSerial()) {
+      checkLEDVal=1;
+    }
   }
-  // Select a card
-  checkLEDVal =1;
+  else
+  {
+    checkLEDVal =0;
+  }
+  
 }
 
 
@@ -333,22 +375,22 @@ void receive_order()
   mfrc522.PCD_StopCrypto1(); // stop encryption on PCD
   content.toUpperCase();
   Serial.println(content);
-  digitalWrite(LED_PIN,HIGH);
-  vTaskDelay(100);
-  digitalWrite(LED_PIN,LOW);
-  vTaskDelay(100);
-  digitalWrite(LED_PIN,HIGH);
-  vTaskDelay(100);
-  digitalWrite(LED_PIN,LOW);
-  vTaskDelay(100);
-  digitalWrite(LED_PIN,HIGH);
-  vTaskDelay(100);
-  digitalWrite(LED_PIN,LOW);
   rfid = "http://172.21.143.74:8080/cmmsservice/repairCheckList/up/VY/2:"+ID+":"+content+"";            
   send_rfid();
   tam3=1;
   tam2=0;
   x=x+1;
+  digitalWrite(LED_PIN,HIGH);
+  vTaskDelay(100);
+  digitalWrite(LED_PIN,LOW);
+  vTaskDelay(100);
+  digitalWrite(LED_PIN,HIGH);
+  vTaskDelay(100);
+  digitalWrite(LED_PIN,LOW);
+  vTaskDelay(100);
+  digitalWrite(LED_PIN,HIGH);
+  vTaskDelay(100);
+  digitalWrite(LED_PIN,LOW);
 }
 void leader()
 {
@@ -358,10 +400,8 @@ void leader()
   digitalWrite(LED_PIN,HIGH);
   vTaskDelay(100);
   digitalWrite(LED_PIN,LOW);
-  while(1)
-   {
-     if (wifiMulti.run() == WL_CONNECTED)
-     { //Check the current connection status
+    // if (wifiMulti.run() == WL_CONNECTED)
+    // { //Check the current connection status
       HTTPClient http;
    
       http.begin("http://172.21.143.74:8080/cmmsservice/repairCheckList/up/VY/1:"+ID+":123456"); //Specify the URL
@@ -378,17 +418,14 @@ void leader()
         Serial.println("Error on HTTP request");
       }
       http.end(); //Free the resources
-      if(httpCode==200) break;
-      }     
-   }
+      //}     
+   
 }
 void material()
 { 
   denhetlieu();
   tam=1;
   dem=millis();
-  while(1)
-   {
      if (wifiMulti.run() == WL_CONNECTED) 
      { //Check the current connection status
       HTTPClient http;
@@ -410,14 +447,10 @@ void material()
         Serial.println("Error on HTTP request");
       }
       http.end(); //Free the resources
-      if(httpCode==200) break;
       }
-   }
 }
 void dead_alive()
 { 
-  while(1)
-   {
      if (wifiMulti.run() == WL_CONNECTED) 
      { //Check the current connection status
       HTTPClient http;
@@ -439,14 +472,10 @@ void dead_alive()
         Serial.println("Error on HTTP request");
       }
       http.end(); //Free the resources
-      if(httpCode==200) break;
       }
-   }
 }
 void send_rfid()
 { 
-   while(1)
-   {
      if (wifiMulti.run() == WL_CONNECTED) 
      { //Check the current connection status
       HTTPClient http;
@@ -465,8 +494,6 @@ void send_rfid()
         Serial.println("Error on HTTP request");
       }
       http.end(); //Free the resources
-      if(httpCode==200) break;
       }
-      Serial.println(rfid);
-   }
+    Serial.println(rfid);
 }
