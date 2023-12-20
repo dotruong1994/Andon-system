8f48cdf33ca1b431f82b3345a06d78f327663689
Do Vu Truong <56952469+dotruong1994@users.noreply.github.com>
2023-12-20 13:00:25 +0700

    Update Light.ino

--- Light.ino
+++ Light.ino
@@ -1,37 +1,49 @@
 void dencanbo() {
+  light_lock = 1;
   for (int i = 0; i < 7; i++) {
     leds[i] = CRGB(0, 0, 255);
     FastLED.show();
   }
+  Serial.println("den can bo");
 }
 void denbaotri() {
+  light_lock = 1;
   for (int i = 0; i < 7; i++) {
     leds[i] = CRGB(0, 255, 20);
     FastLED.show();
   }
+  Serial.println("den bao tri");
 }
 void denhoatdong() {
+  light_lock = 1;
   for (int i = 0; i < 7; i++) {
     leds[i] = CRGB(236, 236, 236);
     FastLED.show();
   }
+  Serial.println("den hoat dong");
 }
 void den_nhan_don() {
+  light_lock = 1;
   for (int i = 0; i < 7; i++) {
     leds[i] = CRGB(255, 0, 0);
     FastLED.show();
   }
+  Serial.println("den nhan don");
 }
 void denhetlieu() {
+  light_lock = 1;
   for (int i = 0; i < 7; i++) {
     leds[i] = CRGB(249, 244, 0);
     FastLED.show();
   } 
+  Serial.println("den het lieu");
 }
 
 void den_trieu_hoi_thanh_dong() {
+  light_lock = 1;
   for (int i = 0; i < 7; i++) {
     leds[i] = CRGB(0, 128, 128);
     FastLED.show();
   }
+  Serial.println("den wifi");
 }
